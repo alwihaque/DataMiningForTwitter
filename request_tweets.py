6576bb3f5db7b570805eda08edbff219508e50b6
enorytk <katietyrone@gmail.com>
2022-04-02 15:43:32 -0400

    setup for tweet lookups

--- request_tweets.py
+++ request_tweets.py
@@ -3,10 +3,6 @@ import os
 import pandas
 import csv
 
-# auth = tweepy.OAuth1UserHandler(
-#    "Iiwmn8OaXO2SroZyhADCqQFCH", "j3e37pZ8GNpG94F3L4wL8LxiB5NYFjQcicyzAInlb6UHuYd6qQ"
-# )
-
 client = tweepy.Client("AAAAAAAAAAAAAAAAAAAAAO42bAEAAAAAOVTkiHTGCVCY5WI557PXbwq7UQ8%3D9vZmiMI98s6tjBRDGWs7GqrEwloYXeC8gl8Id5kmb8U1rynI9e")
 
 files = os.listdir('tweetid')
