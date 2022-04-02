6622475a9ce3ca898c1eff0abc1e0dd822ad1352
enorytk <katietyrone@gmail.com>
2022-04-02 14:39:29 -0400

    help

---
+++ main.py
@@ -0,0 +1,15 @@
+import tweepy
+
+auth = tweepy.OAuth1UserHandler(
+   "Iiwmn8OaXO2SroZyhADCqQFCH", "j3e37pZ8GNpG94F3L4wL8LxiB5NYFjQcicyzAInlb6UHuYd6qQ"
+)
+
+client = tweepy.Client("AAAAAAAAAAAAAAAAAAAAAO42bAEAAAAAOVTkiHTGCVCY5WI557PXbwq7UQ8%3D9vZmiMI98s6tjBRDGWs7GqrEwloYXeC8gl8Id5kmb8U1rynI9e")
+
+#
+# api = tweepy.API(auth)
+#
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print(tweet.text)
+
