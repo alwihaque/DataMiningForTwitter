807e41680d355d4869424b3559c535c093207eb6
alwihaque <61383019+alwihaque@users.noreply.github.com>
2022-04-02 15:00:36 -0400

    Getting My First Tweet

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ auth = tweepy.OAuth1UserHandler(
 
 client = tweepy.Client("AAAAAAAAAAAAAAAAAAAAAO42bAEAAAAAOVTkiHTGCVCY5WI557PXbwq7UQ8%3D9vZmiMI98s6tjBRDGWs7GqrEwloYXeC8gl8Id5kmb8U1rynI9e")
 
+tweets = client.get_tweets(['1333156800926015488'])
+print(tweets.data[0])
 
 #
 # api = tweepy.API(auth)
