3b6a6e8d7a4b9abca8d8e3b24fe5ea96c6a571d7
enorytk <katietyrone@gmail.com>
2022-04-02 15:05:39 -0400

    get files in subfolder

--- request_tweets.py
+++ request_tweets.py
@@ -1,4 +1,8 @@
 import tweepy
+import os
+#api key Iiwmn8OaXO2SroZyhADCqQFCH
+#api secret j3e37pZ8GNpG94F3L4wL8LxiB5NYFjQcicyzAInlb6UHuYd6qQ
+#bearer token AAAAAAAAAAAAAAAAAAAAAO42bAEAAAAAOVTkiHTGCVCY5WI557PXbwq7UQ8%3D9vZmiMI98s6tjBRDGWs7GqrEwloYXeC8gl8Id5kmb8U1rynI9e
 
 auth = tweepy.OAuth1UserHandler(
    "Iiwmn8OaXO2SroZyhADCqQFCH", "j3e37pZ8GNpG94F3L4wL8LxiB5NYFjQcicyzAInlb6UHuYd6qQ"
@@ -9,10 +13,5 @@ client = tweepy.Client("AAAAAAAAAAAAAAAAAAAAAO42bAEAAAAAOVTkiHTGCVCY5WI557PXbwq7
 tweets = client.get_tweets(['1333156800926015488'])
 print(tweets.data[0])
 
-#
-# api = tweepy.API(auth)
-#
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#     print(tweet.text)
-
+files = os.listdir('tweetid')
+print(files)
