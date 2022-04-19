96def4c8c0d4995e53abaef7560f30aeccf01561
enorytk <katietyrone@gmail.com>
2022-04-19 15:59:05 -0400

    Fixed Apriori. Need more data, less days (no double words)

--- apriori_twitter.py
+++ apriori_twitter.py
@@ -1,7 +1,7 @@
-#  THIS  CODE  IS  MY  OWN  WORK,  IT  WAS  WRITTEN
-# WITHOUT CONSULTING CODE WRITTEN BY OTHER STUDENTS. Alwi Haque
 import pandas as pd
 import re
+from dateutil.rrule import rrule, MONTHLY, DAILY
+from datetime import datetime
 # from cleantext import clean
 # import emoji
 
@@ -37,106 +37,117 @@ def give_emoji_free_text(text):
     return text
 
 
-def prune_sub_sets(candidate_list, keys_to_remove):
-    subsets = []
-    for item in candidate_list:
-        for key in keys_to_remove:
-            is_sub_set = set(key).issubset(item)
-            if is_sub_set:
-                subsets.append(item)
-    for subset in subsets:
-        if subset in candidate_list:
-            del candidate_list[subset]
-    return candidate_list
+# def prune_sub_sets(candidate_list, keys_to_remove):
+#     subsets = []
+#     for item in candidate_list:
+#         for key in keys_to_remove:
+#             is_sub_set = set(key).issubset(item)
+#             if is_sub_set:
+#                 subsets.append(item)
+#     for subset in subsets:
+#         if subset in candidate_list:
+#             del candidate_list[subset]
+#     return candidate_list
+#
+#
+# def create_candidate_items(k, item_sets):
+#     first_item_set = list(item_sets[0].keys())
+#     candidate_item_set = {}
+#     if k == 1:
+#         for i in range(len(first_item_set)):
+#             for j in range(len(first_item_set)):
+#                 if first_item_set[i][0] is not first_item_set[j][0]:
+#                     candidate_as_array = [first_item_set[i][0], first_item_set[j][0]]
+#                     candidate = tuple(candidate_as_array)
+#                     candidate_item_set[candidate] = 0
+#         # Remove same combination
+#         # res = {tuple(sorted(x)): y for x, y in candidate_item_set.items()}
+#         candidate_item_set = {tuple(sorted(x)): y for x, y in candidate_item_set.items()}
+#         return candidate_item_set
+#     else:
+#         freq_item_set = list(item_sets[len(item_sets) - 1].keys())
+#         for i in range(len(freq_item_set)):
+#             for j in range(len(first_item_set)):
+#                 candidate_as_array = list(freq_item_set[i])
+#                 if first_item_set[j][0] not in candidate_as_array:
+#                     candidate_as_array.append(first_item_set[j][0])
+#                     candidate = tuple(candidate_as_array)
+#                     candidate_item_set[candidate] = 0
+#         candidate_item_set = {tuple(sorted(x)): y for x, y in candidate_item_set.items()}
+#         return candidate_item_set
 
 
-def create_candidate_items(k, item_sets):
-    first_item_set = list(item_sets[0].keys())
-    candidate_item_set = {}
-    if k == 1:
-        for i in range(len(first_item_set)):
-            for j in range(len(first_item_set)):
-                if first_item_set[i][0] is not first_item_set[j][0]:
-                    candidate_as_array = [first_item_set[i][0], first_item_set[j][0]]
-                    candidate = tuple(candidate_as_array)
-                    candidate_item_set[candidate] = 0
-        # Remove same combination
-        # res = {tuple(sorted(x)): y for x, y in candidate_item_set.items()}
-        candidate_item_set = {tuple(sorted(x)): y for x, y in candidate_item_set.items()}
-        return candidate_item_set
-    else:
-        freq_item_set = list(item_sets[len(item_sets) - 1].keys())
-        for i in range(len(freq_item_set)):
-            for j in range(len(first_item_set)):
-                candidate_as_array = list(freq_item_set[i])
-                if first_item_set[j][0] not in candidate_as_array:
-                    candidate_as_array.append(first_item_set[j][0])
-                    candidate = tuple(candidate_as_array)
-                    candidate_item_set[candidate] = 0
-        candidate_item_set = {tuple(sorted(x)): y for x, y in candidate_item_set.items()}
-        return candidate_item_set
-
-
-def apriori_algorithm(input_file, min_sup_count, output_filename, date):
+def apriori_algorithm(input_file, threshold, output_filename, date):
     data = pd.read_csv(input_file)
     data = data[data["date"] == date]
-    keywords = data.body.tolist()
-    item_set = {}
-    k = 1
-    frequency_list = []
-    for keyword in keywords:
-        # remove emojis from keyword
-        give_emoji_free_text(keyword)
-        each_keyword = keyword.split(' ')
-        for word in each_keyword:
-            word_array = [word]
-            if tuple(word_array) in item_set:
-                item_set[tuple(word_array)] = item_set.get(tuple(word_array)) + 1
+    data["body"] = data['body'].apply(lambda x: give_emoji_free_text(x))
+    # print(data['body'])
+    data["keywords"] = data["body"].str.split(" ", -1, False)
+    # print(data["keywords"])
+    # print(keywords)
+    frequency = {}
+    superset = []
+
+    # Get initial k=1 frequency set (need to iterate over every word in every row)
+    for index, row in data['keywords'].iteritems():
+        # print(tweet)
+        superset.append(frozenset(row))
+        for item in row:
+            item = frozenset([item])
+            if item in frequency:
+                frequency[item] = frequency[item] + 1
             else:
-                item_set[tuple(word_array)] = 1
-    keys_to_remove = []
-    for item in item_set:
-        if item_set[item] < min_sup_count:
-            keys_to_remove.append(item)
-    for key in keys_to_remove:
-        del item_set[key]
-    frequency_list.append(item_set)
-    while True:
-        candidate_list = create_candidate_items(k, frequency_list)
-        candidate_list = prune_sub_sets(candidate_list, keys_to_remove)
-        for item in candidate_list:
-            for word in keywords:
-                item_as_array = list(item)
-                word_array = word.split(';')
-                if all(it in word_array for it in item_as_array):
-                    candidate_list[item] = candidate_list.get(item) + 1
-        for item in candidate_list:
-            if candidate_list[item] < min_sup_count:
-                keys_to_remove.append(item)
-        for key in keys_to_remove:
-            if key in candidate_list:
-                del candidate_list[key]
-        if len(candidate_list) == 0:
-            break
-        frequency_list.append(candidate_list)
+                frequency[item] = 1
+
+    # Create dictionaries of frequent and not frequent items
+    not_frequent = {key: val for key, val in frequency.items() if val < threshold}
+    frequency = {key: val for key, val in frequency.items() if val >= threshold}
+    out_put = {key: val for key, val in frequency.items()}
+
+    frequency_k = frequency.copy()
+
+    k = 2
+    while len(frequency_k.keys()) > 0:
+        candidate = []
+        # Generate candidates, ignoring duplicates
+        for index, key in enumerate(frequency):
+            for i, l in enumerate(frequency_k):
+                if key.union(l) not in candidate:
+                    candidate.append(key.union(l))
+
+        # Prune candidates for subsets and make sure they are the correct length
+        candidate = [x for x in candidate if x not in not_frequent and len(x) == k]
+        frequency_k = {}
+        for item in candidate:
+            frequency_k[item] = 0
+            for i in superset:
+                if item.issubset(i):
+                    frequency_k[item] = frequency_k[item] + 1
+        # Not frequent and frequent k candidates
+        not_frequent = {key: val for key, val in frequency_k.items() if val < threshold}
+        frequency_k = {key: val for key, val in frequency_k.items() if val >= threshold}
+        for index, key in enumerate(frequency_k):
+            out_put[key] = frequency_k[key]
         k = k + 1
-    final_list = []
-    for candidate in frequency_list:
-        for value in candidate:
-            value = [value, candidate[value]]
-            final_list.append(value)
-    final_list = sorted(final_list, key=lambda x: x[1], reverse=True)
 
     with open(output_filename, 'w') as f:
-        for value in final_list:
-            for word in value[0]:
-                f.write(word)
-                f.write(" ")
-            f.write("(")
-            f.write(str(value[1]))
-            f.write(")")
-            f.write("\n")
+        ordered_freq = sorted(frequency, key=frequency.get, reverse=True)
+        st = ""
+        for k in ordered_freq:
+            st += " ".join(k) + " (" + str(frequency[k]) + ")\n"
+        f.write(st)
 
+# apriori_algorithm("tweets.csv", 200, "output.txt", date)
+if __name__ == '__main__':
+    #DEC 2, 25, 29, 30
+    ignore = [datetime(2021,12,2), datetime(2021,12,25), datetime(2021,12,29), datetime(2021,12,30)]
+    #date format in 2021-12-10 in csv
+    start_date = datetime(2021, 12, 1)
+    end_date = datetime(2022, 1, 31)
 
+    for dt in rrule(freq=DAILY,dtstart=start_date, until=end_date):
+        if dt in ignore:
+            continue
+        date = dt.strftime("%Y-%m-%d")
+        apriori_algorithm("tweets.csv", 50, "apr_"+date+".csv", date)
 
-# apriori_algorithm("tweets.csv", 200, "output.txt")
