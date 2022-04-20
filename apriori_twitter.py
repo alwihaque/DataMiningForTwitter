318d63edf9278e8c71e52b6d8dd33f9b6a5e06a0
enorytk <katietyrone@gmail.com>
2022-04-20 19:22:50 -0400

    Multiple words and apriori results for thres 65

--- apriori_twitter.py
+++ apriori_twitter.py
@@ -34,52 +34,12 @@ def give_emoji_free_text(text):
     text = text.lower()
     text = re.sub(r"http\S+", "", text)
     text = re.sub("[^a-z0-9@#]"," ", text)
+    text = re.sub('\s+(a|an|and|the|s|t)(\s+)', '\2', text)
     return text
-
-
-# def prune_sub_sets(candidate_list, keys_to_remove):
-#     subsets = []
-#     for item in candidate_list:
-#         for key in keys_to_remove:
-#             is_sub_set = set(key).issubset(item)
-#             if is_sub_set:
-#                 subsets.append(item)
-#     for subset in subsets:
-#         if subset in candidate_list:
-#             del candidate_list[subset]
-#     return candidate_list
-#
-#
-# def create_candidate_items(k, item_sets):
-#     first_item_set = list(item_sets[0].keys())
-#     candidate_item_set = {}
-#     if k == 1:
-#         for i in range(len(first_item_set)):
-#             for j in range(len(first_item_set)):
-#                 if first_item_set[i][0] is not first_item_set[j][0]:
-#                     candidate_as_array = [first_item_set[i][0], first_item_set[j][0]]
-#                     candidate = tuple(candidate_as_array)
-#                     candidate_item_set[candidate] = 0
-#         # Remove same combination
-#         # res = {tuple(sorted(x)): y for x, y in candidate_item_set.items()}
-#         candidate_item_set = {tuple(sorted(x)): y for x, y in candidate_item_set.items()}
-#         return candidate_item_set
-#     else:
-#         freq_item_set = list(item_sets[len(item_sets) - 1].keys())
-#         for i in range(len(freq_item_set)):
-#             for j in range(len(first_item_set)):
-#                 candidate_as_array = list(freq_item_set[i])
-#                 if first_item_set[j][0] not in candidate_as_array:
-#                     candidate_as_array.append(first_item_set[j][0])
-#                     candidate = tuple(candidate_as_array)
-#                     candidate_item_set[candidate] = 0
-#         candidate_item_set = {tuple(sorted(x)): y for x, y in candidate_item_set.items()}
-#         return candidate_item_set
-
 
 def apriori_algorithm(input_file, threshold, output_filename, date):
     data = pd.read_csv(input_file)
-    # data = data[data["date"] == date]
+    data = data[data["date"] == date]
     data["body"] = data['body'].apply(lambda x: give_emoji_free_text(x))
     # print(data['body'])
     data["keywords"] = data["body"].str.split(" ", -1, False)
@@ -93,6 +53,8 @@ def apriori_algorithm(input_file, threshold, output_filename, date):
         # print(tweet)
         superset.append(frozenset(row))
         for item in row:
+            if item == " " or item =="":
+                continue
             item = frozenset([item])
             if item in frequency:
                 frequency[item] = frequency[item] + 1
@@ -108,6 +70,7 @@ def apriori_algorithm(input_file, threshold, output_filename, date):
 
     k = 2
     while len(frequency_k.keys()) > 0:
+        # print(k)
         candidate = []
         # Generate candidates, ignoring duplicates
         for index, key in enumerate(frequency):
@@ -131,23 +94,24 @@ def apriori_algorithm(input_file, threshold, output_filename, date):
         k = k + 1
 
     with open(output_filename, 'w') as f:
-        ordered_freq = sorted(frequency, key=frequency.get, reverse=True)
+        ordered_freq = sorted(out_put, key=out_put.get, reverse=True)
         st = ""
         for k in ordered_freq:
-            st += " ".join(k) + " (" + str(frequency[k]) + ")\n"
+            st += " ".join(k) + " (" + str(out_put[k]) + ")\n"
         f.write(st)
 
 # apriori_algorithm("tweets.csv", 200, "output.txt", date)
 if __name__ == '__main__':
     #DEC 2, 25, 29, 30
-    ignore = [datetime(2021,12,2), datetime(2021,12,25), datetime(2021,12,29), datetime(2021,12,30)]
+    ignore = [datetime(2021,12,2), datetime(2021,12,7), datetime(2021,12,20), datetime(2021,12,29), datetime(2021,12,30), datetime(2022,1,13), datetime(2022,1,15), datetime(2022,1,22)]
     #date format in 2021-12-10 in csv
     start_date = datetime(2021, 12, 1)
     end_date = datetime(2022, 1, 31)
-    apriori_algorithm("tweets.csv", 700, "all_data_apriori.csv", 0)
-    # for dt in rrule(freq=DAILY,dtstart=start_date, until=end_date):
-    #     if dt in ignore:
-    #         continue
-    #     date = dt.strftime("%Y-%m-%d")
-    #     apriori_algorithm("tweets.csv", 50, "apr_"+date+".csv", date)
+    # apriori_algorithm("tweets.csv", 10000, "all_data_apriori.csv", 0)
+    for dt in rrule(freq=DAILY,dtstart=start_date, until=end_date):
+        if dt in ignore:
+            continue
+        date = dt.strftime("%Y-%m-%d")
+        print(date)
+        apriori_algorithm("tweets.csv", 65, "apriori/apr_"+date+".csv", date)
 
